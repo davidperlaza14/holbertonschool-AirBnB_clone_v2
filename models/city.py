a627a65126d262e41425520e1c8fe5be6d2ef261
davidperlaza14 <davidperlaza14@gmail.com>
2022-12-02 22:26:18 -0500

    6. DBStorage - States and Cities

--- models/city.py
+++ models/city.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-import models
-from models.place import Place
+from os import getenv
 from models.base_model import BaseModel, Base
-import sqlalchemy
+# from models.state import State
+import models
+from sqlalchemy import Column, String, Integer, ForeignKey
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
 
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(128), ForeignKey('states.id'), nullable=False)
-    places = relationship("Place", backref="cities", cascade='delete')
-
-    def __init__(self, *args, **kwargs):
-        """Initialization inheroted"""
-        super().__init__(*args, **kwargs)
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    else:
+        name = ''
+        state_id = ''
