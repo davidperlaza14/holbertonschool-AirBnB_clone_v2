fa9c8713bad8d1d1b297a930f9ec275efc176357
davidperlaza14 <davidperlaza14@gmail.com>
2022-12-02 21:41:18 -0500

    6. DBStorage - States and Cities

--- models/city.py
+++ models/city.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel, Base
+import models
 from models.place import Place
-from sqlalchemy import Column, String, ForeignKey
+from models.base_model import BaseModel, Base
+import sqlalchemy
 from sqlalchemy.orm import relationship
-from os import getenv as env
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
 
 
-class City(BaseModel, Base if (env("HBNB_TYPE_STORAGE") == "db") else object):
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
+    __tablename__ = 'cities'
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(128), ForeignKey('states.id'), nullable=False)
+    places = relationship("Place", backref="cities", cascade='delete')
 
-    if env("HBNB_TYPE_STORAGE") == "db":
-        __tablename__ = 'cities'
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-        name = Column(String(128), nullable=False)
-        places = relationship("Place", cascade="all, delete", backref="cities")
-
-    else:
-        name = ""
-        state_id = ""
+    def __init__(self, *args, **kwargs):
+        """Initialization inheroted"""
+        super().__init__(*args, **kwargs)
--- models/state.py
+++ models/state.py
@@ -1,29 +1,31 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from models.city import City
 import models
+from models.city import City
+from models.base_model import BaseModel, Base
+import sqlalchemy
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, String
-from os import getenv as env
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
 
 
-class State(BaseModel, Base if (env("HBNB_TYPE_STORAGE") == "db") else object):
+class State(BaseModel, Base):
     """ State class """
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state")
 
-    if env("HBNB_TYPE_STORAGE") == "db":
-        __tablename__ = 'states'
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", cascade="all, delete", backref="state")
+    def __init__(self, *args, **kwargs):
+        """initialize inherited"""
+        super().__init__(*args, **kwargs)
 
-    else:
-        name = ""
-
+    if models.storage_type != "db":
         @property
         def cities(self):
-            """Returns the list of City instances"""
-            new_list = []
-            for city in list(models.storage.all(City).values()):
-                if city.state_id == self.id:
-                    new_list.append(city)
-            return new_lis
+            """getter"""
+            list_city = []
+            all_inst_C = models.String.all(City)
+            for value in all_inst_C.values():
+                if value.state_id == self.id:
+                    list_city.append(value)
+            return list_city
