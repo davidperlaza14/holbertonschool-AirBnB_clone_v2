fae26f3c5437b1555d863a3995f4dc771924da4d
davidperlaza14 <davidperlaza14@gmail.com>
2022-12-09 17:55:12 -0500

    7. Improve engines

--- models/state.py
+++ models/state.py
@@ -1,37 +1,33 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from models.city import City
+"""Defines the State class."""
 import models
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
 from os import getenv
-""" `getenv` to determine in which storage type we are
-by scanning the HBNB_TYPE_STORAGE"""
+from models.base_model import Base
+from models.base_model import BaseModel
+from models.city import City
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
+    """Represents a state for a MySQL database.
+    Inherits from SQLAlchemy Base and links to the MySQL table states.
+    Attributes:
+        __tablename__ (str): The name of the MySQL table to store States.
+        name (sqlalchemy String): The name of the State.
+        cities (sqlalchemy relationship): The State-City relationship.
     """
-    State class
-    Establish a relationship with the class City
-    """
-    __tablename__ = 'states'
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        """ db ==>  means let's go for SQLAlchemy logic"""
-        name = Column(String(128), nullable=False)
-        cities = relationship('City', cascade='all, delete', backref='state')
-    else:
-        name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City",  backref="state", cascade="delete")
 
-    @property
-    def cities(self):
-        """
-        Getter - returns the list of City instances
-        with state_id == State.id
-        FileStorage relationship between State and City
-        """
-        l_cities = []
-        for city in models.storage.all(City).values():
-            if city.state_id == self.id:
-                l_cities.append(city)
-        return l_cities
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """Get a list of all related City objects."""
+            city_list = []
+            for city in list(models.storage.all(City).values()):
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
