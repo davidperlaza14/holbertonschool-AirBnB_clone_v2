b77902955dfa634ea4da442b60bfb098942d9e4f
davidperlaza14 <davidperlaza14@gmail.com>
2022-12-11 12:02:29 -0500

    5. Delete object

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,16 +1,35 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
     __file_path = 'file.json'
     __objects = {}
+    classes = {
+               'BaseModel': BaseModel, 'User': User, 'Place': Place,
+               'State': State, 'City': City, 'Amenity': Amenity,
+               'Review': Review
+              }
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if not cls:
+            return FileStorage.__objects
+        return {key: object_ for key, object_ in FileStorage.__objects.items() if isinstance(object_,cls)}
+
+    def delete(self, obj=None):
+        if obj:
+            del(FileStorage.__objects[obj.to_dict()['__class__'] + '.' + obj.id])
+
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
