ef22157b6f2ced19155e8c04498fa0098010cacd
davidperlaza14 <davidperlaza14@gmail.com>
2022-12-11 11:28:29 -0500

    4. MySQL setup test

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,5 +1,4 @@
--- create databesa and user for this project
--- create test case for this prroject
+-- create test databese and user for this project
 CREATE SCHEMA IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
