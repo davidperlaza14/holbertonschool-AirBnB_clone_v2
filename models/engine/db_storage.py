07c6769bdacea29b51aad7140a0b72c0a216fd30
davidperlaza14 <davidperlaza14@gmail.com>
2022-12-11 20:38:56 -0500

    6. DBStorage - States and Cities

---
+++ models/engine/db_storage.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python3
+"""Defines the DBStorage engine."""
+from os import getenv
+from models.base_model import Base
+from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from sqlalchemy import create_engine
+from sqlalchemy.orm import relationship
+from sqlalchemy.orm import scoped_session
+from sqlalchemy.orm import sessionmaker
+
+
+class DBStorage:
+    """
+    Create our database with SQLAlchemy
+    Alchemy is our best friend!
+    """
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        """
+        Starting the engine
+        """
+        user = getenv('HBNB_MYSQL_USER')
+        pwd = getenv('HBNB_MYSQL_PWD')
+        host = getenv('HBNB_MYSQL_HOST')
+        database = getenv('HBNB_MYSQL_DB')
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
+                                      .format(user, pwd, host, database),
+                                      pool_pre_ping=True)
+        if getenv('HBNB_ENV') == 'test':
+            Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        """
+        Perform query on the current database session
+        # Must return a dictionary with all objects according
+        to class name passed in cls argument
+        """
+        obj_dict = {}
+        if cls != '':
+            objs = self.__session.query(cls)
+        else:
+            objs = self.__session.query(Amenity)
+            # We could have used extend() list method too,
+            # but would have needed another way to code also
+            objs += self.__session.query(City)
+            objs += self.__session.query(Place)
+            objs += self.__session.query(Review)
+            objs += self.__session.query(State)
+            objs += self.__session.query(User)
+        return {"{}.{}".format(obj.__class__.__name__, obj.id): obj
+                for obj in objs}
+
+    def new(self, obj):
+        """Adds the object to the current db session"""
+        self.__session.add(obj)
+
+    def save(self):
+        """Commit all changes to current db session"""
+        self.__session.commit()
+
+    def delete(self, obj):
+        """ Delete obj of current db session """
+        if obj:
+            self.__session.delete(obj)
+
+    def reload(self):
+        """
+        Commit all changes in database after
+        the changings
+        """
+        Base.metadata.create_all(self.__engine)
+        session_factory = sessionmaker(
+            bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(session_factory)
+        self.__session = Session
+
+    # def close(self):
+    #     """close session, proper ending"""
+    #     self.__session.remove()
+
+    #def classes(self):
+        """ returns dictionary of valid classes """
--- models/state.py
+++ models/state.py
@@ -1,8 +1,25 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+""" City Module for HBNB project """
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+import os
 
+HBNB_TYPE_STORAGE = os.environ['HBNB_TYPE_STORAGE']
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base if HBNB_TYPE_STORAGE == 'db' else object):
+    """ The State  class """
+    if HBNB_TYPE_STORAGE == 'db':
+        __tablename__ = 'states'
+        name= Column(String(128), nullable=False)
+        cities = relationship("City", bockref="state", coscode="delete")
+    else:
+        name = ""
+
+        @property
+        def cities(self):
+            from models.city import City
+            from models import storage
+
+            city_objects =storage.all(City)
+            return [city for city in city_objects.values() if city.state_id == self.id]
